4abd90306d61c68ca304d4e522b5e4054d881c56
Scott Leishman <scott.leishman@gmail.com>
2010-03-08 11:12:31 -0800

    added color prompt and xterm title display when possible.

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,4 +1,27 @@
-# setup the display of the command prompt
+# setup the display of the command prompt and window title
 
-#plain-vanilla [user@host:cwd]$
-export PS1='[\u@\h:\w]$'
+if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+    #we have color support, assume we want a color prompt
+    color_prompt=yes
+else
+    color_prompt=
+fi
+
+if [ "$color_prompt" = yes ]; then
+    #[light green user@host:light blue cwd]$
+    PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$'
+else
+    #plain-vanilla [user@host:cwd]$
+    PS1='[\u@\h:\w]$'
+fi
+
+case $TERM in 
+xterm*|rxvt*)
+    #xterm-like terminal, display user@host:cwd in title bar
+    PS1="\[\033]0;\u@\h:\w\007\]$PS1"
+    ;;
+*)
+    ;;
+esac
+        
+export PS1
